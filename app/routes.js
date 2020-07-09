634ce9af0247608f038396dfb0a0973ef03c7164
Guy Tristram <guy.tristram@digital.trade.gov.uk>
2020-07-09 17:04:26 +0100

    most of the validation is done

--- app/routes.js
+++ app/routes.js
@@ -42,17 +42,60 @@ router.post( '/noticeofchange/solicitor/startorstop', function (req, res)
 // NOTICE OF CHANGE
 router.post( '/noticeofchange/solicitor/addenterdetails', function (req, res)
 {
-    res.redirect('/noticeofchange/solicitor/selectroleprobate');
+    //console.warn("Main Router " + req.session.data['casenumber'].length);
+    if(req.session.data['casenumber'].length < 16  ||  20 < req.session.data['casenumber'].length )
+    {
+        req.session.data['errorcasenumber'] = 'true';
+        //console.warn("Main Router " + req.session.data['errorcasenumber']);
+        res.redirect('addenterdetails');
+    }
+    else
+    {
+        req.session.data['errorcasenumber'] = 'false';
+        req.session.data['role'] = 'undefined';
+        res.redirect('/noticeofchange/solicitor/selectroleprobate');
+    }
 })
 
 router.post( '/noticeofchange/solicitor/selectroleprobate', function (req, res)
 {
-    res.redirect('/noticeofchange/solicitor/securityquestionprobate');
+    if(req.session.data['role'] == 'undefined' )
+    {
+        req.session.data['errorrole'] = 'true';
+        res.redirect('selectroleprobate');
+    }
+    else
+    {
+        req.session.data['errorrole'] = 'false';
+        req.session.data['role'] = 'lastname';
+        res.redirect('/noticeofchange/solicitor/securityquestionprobate');
+    }
 })
 
 router.post( '/noticeofchange/solicitor/securityquestionprobate', function (req, res)
 {
-    res.redirect('/noticeofchange/solicitor/checkanswers');
+    console.warn("Main Router  " + req.session.data['lastname']);
+    if(req.session.data['lastname'] == 'undefined' )
+    {
+        req.session.data['errorsecurityprobate'] = 'true';
+        res.redirect('securityquestionprobate');
+    }
+    else
+    {
+        if(req.session.data['lastname'] != 'Narran'  &&  req.session.data['lastname'] != 'narran' )
+        {
+            req.session.data['errorsecurityprobatewrongname'] = 'true';
+            res.redirect('securityquestionprobate');
+        }
+        else
+        {
+            req.session.data['errorsecurityprobate'] = 'false';
+            req.session.data['errorsecurityprobatewrongname'] = 'false';
+            res.redirect('/noticeofchange/solicitor/checkanswers');
+        }
+    }
+
+
 })
 
 router.post( '/noticeofchange/solicitor/checkanswers', function (req, res)
