cc20906da53dd8955e9a075ad0142e0ef0e7aed4
Guy Tristram <guy.tristram@digital.trade.gov.uk>
2020-08-04 11:45:40 +0100

    Archive of V2 testing

--- app/routes.js
+++ app/routes.js
@@ -44,7 +44,9 @@ router.post( '/noticeofchange/solicitor/startorstop', function (req, res)
 
 
 // NOTICE OF CHANGE
-// Version 1
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// Version 1 - No longer in use
 router.get( '/noticeofchange/v1/startnoc', function (req, res)
 {
     req.session.data['casenumber'] = '';
@@ -215,7 +217,251 @@ router.post( '/noticeofchange/v1/solicitor/selectrole', function (req, res)
 
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+// Version 2 - No longer in use
+router.get( '/noticeofchange/v2/startnoc', function (req, res)
+{
+    req.session.data['casenumber'] = '';
+    req.session.data['errorcasenumber'] = 'false';
 
+    req.session.data['role'] == 'undefined';
+    req.session.data['errorrole'] = 'false';
+
+    req.session.data['lastname'] == 'undefined'
+    req.session.data['errorsecurityprobate'] = 'false';
+    req.session.data['errorsecurityprobatewrongname'] = 'false';
+    req.session.data['sot'] = 'undefined';
+
+    req.session.data['day'] = '';
+    req.session.data['month'] = '';
+    req.session.data['year'] = '';
+    req.session.data['errorsecuritydivorce'] = 'false';
+
+    req.session.data['errorsot'] = 'false';
+
+    res.redirect('/noticeofchange/v2/solicitor/addenterdetails');
+})
+
+router.post( '/noticeofchange/v2/solicitor/addenterdetails', function (req, res)
+{
+    //console.warn("Main Router " + req.session.data['casenumber'].length);
+    if(req.session.data['casenumber'].length < 16  ||  20 < req.session.data['casenumber'].length )
+    {
+        req.session.data['errorcasenumber'] = 'true';
+        //console.warn("Main Router " + req.session.data['errorcasenumber']);
+        res.redirect('addenterdetails');
+    }
+    else
+    {
+        req.session.data['errorcasenumber'] = 'false';
+        req.session.data['role'] = 'undefined';
+
+
+        if(req.session.data['caselist'] == 'probate')
+        {
+            res.redirect('/noticeofchange/v2/solicitor/selectroleprobate');
+        }
+        else
+        {
+            req.session.data['caselist'] = 'divorce';
+            res.redirect('/noticeofchange/v2/solicitor/selectroledivorce');
+        }
+    }
+})
+
+router.post( '/noticeofchange/v2/solicitor/selectroleprobate', function (req, res)
+{
+    if(req.session.data['role'] == 'undefined' )
+    {
+        req.session.data['errorrole'] = 'true';
+        res.redirect('selectroleprobate');
+    }
+    else
+    {
+        req.session.data['errorrole'] = 'false';
+
+        res.redirect('/noticeofchange/v2/solicitor/securityquestionprobate');
+    }
+})
+
+router.post( '/noticeofchange/v2/solicitor/selectroledivorce', function (req, res)
+{
+    if(req.session.data['role'] == 'undefined' )
+    {
+        req.session.data['errorrole'] = 'true';
+        res.redirect('selectroledivorce');
+    }
+    else
+    {
+        req.session.data['errorrole'] = 'false';
+
+        res.redirect('/noticeofchange/v2/solicitor/securityquestiondivorce');
+    }
+})
+
+router.post( '/noticeofchange/v2/solicitor/securityquestionprobate', function (req, res)
+{
+    console.warn("Main Router  " + req.session.data['lastname']);
+    if(req.session.data['lastname'] == 'undefined' )
+    {
+        req.session.data['errorsecurityprobate'] = 'true';
+        res.redirect('securityquestionprobate');
+    }
+    else
+    {
+        if(req.session.data['lastname'] != 'Narran'  &&  req.session.data['lastname'] != 'narran' )
+        {
+            req.session.data['errorsecurityprobatewrongname'] = 'true';
+            res.redirect('securityquestionprobate');
+        }
+        else
+        {
+            req.session.data['errorsecurityprobate'] = 'false';
+            req.session.data['errorsecurityprobatewrongname'] = 'false';
+            req.session.data['sot'] = 'undefined';
+            res.redirect('/noticeofchange/v2/solicitor/checkanswers');
+        }
+    }
+})
+
+router.post( '/noticeofchange/v2/solicitor/securityquestiondivorce', function (req, res)
+{
+    console.warn("Main Router  " + req.session.data['day']);
+    if(req.session.data['day'] == ''  ||
+        req.session.data['month'] == '' ||
+        req.session.data['year'] == ''  )
+    {
+        req.session.data['errorsecuritydivorce'] = 'true';
+        res.redirect('securityquestiondivorce');
+    }
+    else
+    {
+        var months = ['filler', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+        req.session.data['monthoutput'] = months[req.session.data['month']];
+        console.warn("Main Router " + req.session.data['monthoutput']);
+
+        req.session.data['errorsecuritydivorce'] = 'false';
+        req.session.data['sot'] = 'undefined';
+        res.redirect('/noticeofchange/v2/solicitor/checkanswers');
+    }
+})
+
+router.post( '/noticeofchange/v2/solicitor/checkanswers', function (req, res)
+{
+    console.warn("Main Router +" + req.session.data['sot']  + "+++");
+    console.warn("****************************");
+    if(req.session.data['solicitorhasalready'] == 'true' )
+    {
+        // where solicitor in firm already has case
+        res.redirect('alreadygotcaseerror');
+    }
+    else if(req.session.data['sot'] != 'on' )
+    {
+        req.session.data['errorsot'] = 'true';
+        res.redirect('checkanswers');
+    }
+    else
+    {
+        // Sort out the data and time of right now
+        var months = ['filler', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+        var currentDate = new Date()
+        var day = currentDate.getDate()
+        var monthtoday = currentDate.getMonth() + 1
+        var year = currentDate.getFullYear()
+
+        var timenowis = currentDate.toLocaleString('en-US', { hour: 'numeric',  minute: 'numeric', second: 'numeric', hour12: true })
+
+        req.session.data['todaydate'] = day + " " + months[monthtoday] + " " + year;
+        req.session.data['todaydatetime'] = timenowis;
+        req.session.data['errorsot'] = 'false';
+        req.session.data['casenumberselected'] = req.session.data['casenumber'];
+        req.session.data['caseadded'] = 'true';
+        req.session.data['adminonly'] = 'true';
+        res.redirect('/noticeofchange/v2/solicitor/confirmation?autoapproved=true&adminonly=false&servicedown=false&');
+    }
+})
+
+
+
+// STOP REPRESENTING CLIENT
+router.post( '/noticeofchange/v2/solicitor/casedetailsprobate', function (req, res)
+{
+    req.session.data['stopstop'] = '';
+
+    res.redirect('/noticeofchange/v2/solicitor/stopselectclients');
+})
+
+router.post( '/noticeofchange/v2/solicitor/casedetailsdivorce', function (req, res)
+{
+    req.session.data['stopstop'] = '';
+    req.session.data['stoppingmoredetail'] = '';
+    console.warn("stop checkbox +" + req.session.data['stopstop']  + "+++");
+
+    res.redirect('/noticeofchange/v2/solicitor/stopselectclients');
+})
+
+router.post( '/noticeofchange/v2/solicitor/casedetailsimmigration', function (req, res)
+{
+    req.session.data['stopstop'] = '';
+    req.session.data['stoppingmoredetail'] = '';
+    console.warn("stop checkbox +" + req.session.data['stopstop']  + "+++");
+
+    res.redirect('/noticeofchange/v2/solicitor/stopselectclients');
+})
+
+router.post( '/noticeofchange/v2/solicitor/casedetailspubliclaw', function (req, res)
+{
+    req.session.data['stopstop'] = '';
+    req.session.data['stoppingmoredetail'] = '';
+    console.warn("stop checkbox +" + req.session.data['stopstop']  + "+++");
+
+    res.redirect('/noticeofchange/v2/solicitor/stopselectclients');
+})
+
+router.post( '/noticeofchange/v2/solicitor/stopselectclients', function (req, res)
+{
+    if(req.session.data['clientselected'] != 'on' )
+    {
+        req.session.data['errorstopnoperson'] = 'true';
+        res.redirect('stopselectclients');
+    }
+    else
+    {
+        req.session.data['errorstopnoperson'] = 'false';
+        res.redirect('/noticeofchange/v2/solicitor/stopreason');
+    }
+
+})
+
+router.post( '/noticeofchange/v2/solicitor/stopreason', function (req, res)
+{
+    res.redirect('/noticeofchange/v2/solicitor/confirmstop');
+})
+
+router.post( '/noticeofchange/v2/solicitor/confirmstop', function (req, res)
+{
+    console.warn("stop checkbox +" + req.session.data['stopstop']  + "+++");
+
+    console.warn("case to hide  +" + req.session.data['casetohide']  + "+++");
+    if (req.session.data['casetohide'] == 'a')
+    {
+        req.session.data['hideone'] = 'true';
+    }
+    else if (req.session.data['casetohide'] == 'b')
+    {
+        req.session.data['hidetwo'] = 'true';
+    }
+    else if (req.session.data['casetohide'] == 'c')
+    {
+        req.session.data['hidethree'] = 'true';
+    }
+
+    // Redirect to correct confirmation style
+    res.redirect('/noticeofchange/v2/solicitor/confirmationofstopping');
+
+
+
+})
 
 
 
@@ -342,8 +588,17 @@ router.post( '/noticeofchange/solicitor/securityquestiondivorce', function (req,
     }
     else
     {
+        req.session.data['tempmonthdateformat'] = req.session.data['month'];
+        //work with 01 month format
+        console.warn("Date substring is :  " +  req.session.data['tempmonthdateformat'].substring(0, 1));
+        if(req.session.data['tempmonthdateformat'].substring(0, 1) == '0')
+        {
+            req.session.data['tempmonthdateformat'] = req.session.data['tempmonthdateformat'].substring(1, 2)
+            console.warn("BRAND NEW  date is :  " +  req.session.data['tempmonthdateformat']);
+        }
+
         var months = ['filler', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
-        req.session.data['monthoutput'] = months[req.session.data['month']];
+        req.session.data['monthoutput'] = months[req.session.data['tempmonthdateformat']];
         console.warn("Main Router " + req.session.data['monthoutput']);
 
         req.session.data['errorsecuritydivorce'] = 'false';
